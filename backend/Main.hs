dd27571a9572c57314d81f6de50f067270b0fdd6
Dan Rosén <danr@student.chalmers.se>
2013-01-16 21:56:57 +0100

    Dummy-support /submit route in backend

--- backend/Main.hs
+++ backend/Main.hs
@@ -1,11 +1,41 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings, DeriveGeneric, GeneralizedNewtypeDeriving, ViewPatterns #-}
 module Main where
 
+import Prelude hiding (words)
 import Data.Monoid (mconcat)
 
 import Network.Wai.Middleware.RequestLogger (logStdout)
 import Network.Wai.Middleware.Static
 import Web.Scotty
+
+import Data.Aeson hiding (json)
+import GHC.Generics
+
+data Submit = Submit
+    { snake :: [[Int]]
+    , user  :: String
+    }
+  deriving Generic
+
+instance FromJSON Submit
+
+data Response = Response
+    { correct :: Bool
+    , score   :: Int
+    , words   :: Int
+    }
+  deriving Generic
+
+instance ToJSON Response
+
+type Coord = (Int,Int)
+type Snake = [Coord]
+
+submitSnake :: Submit -> Snake
+submitSnake = map (\[x,y] -> (x,y)) . snake
+
+ok :: Snake -> Bool
+ok _ = True
 
 main :: IO ()
 main = scotty 3000 $ do
@@ -17,4 +47,13 @@ main = scotty 3000 $ do
         , foldr1 (<|>) (map hasSuffix
             ["html", "js", "css", "jpg", "txt"])
         ] <|> only (zip ["","/"] (repeat "frontend/index.html"))
+
+
+    post "/submit" $ do
+        Just submit <- jsonData
+        json $ Response
+            { correct = ok (submitSnake submit)
+            , score   = 100
+            , words   = 1
+            }
 
